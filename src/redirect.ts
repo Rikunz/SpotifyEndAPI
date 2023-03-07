960aebbeb28db674f8ebad6bb521c8aeb9e852ac
Thoriq Firdaus Arifin <88525167+Rikunz@users.noreply.github.com>
2023-03-07 21:39:19 +0700

    Update redirect.ts

--- src/redirect.ts
+++ src/redirect.ts
@@ -3,7 +3,7 @@ async function get_curent_track() {
         method: "POST",
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Basic ' + (new Buffer('5bb41046b4274f9eb53486c7315cede8' + ':' + 'd4fe42cccc1d4ba48cda568eb4ad2dde').toString('base64'))
+            'Authorization': 'Basic ' + (new Buffer('ClientID' + ':' + 'Secret').toString('base64'))
         },
     });
     const data = await response.json()
